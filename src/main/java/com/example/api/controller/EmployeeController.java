379179a7de229cd775b20d99a5940c034f686d9d
Cédric <cedric.jamme@sciores.fr>
2022-09-28 16:53:53 +0200

    add mapping id

--- src/main/java/com/example/api/controller/EmployeeController.java
+++ src/main/java/com/example/api/controller/EmployeeController.java
@@ -1,15 +1,18 @@
 package com.example.api.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.model.Employee;
-
+import com.example.api.model.EmployeeCategory;
 import com.example.api.service.EmployeeService;
 
 @RestController
@@ -28,6 +31,11 @@ public class EmployeeController {
         return employeeService.getEmployees();
     }
 
+    @GetMapping("/employees/{id}")
+    public Optional<Employee> getEmployee(long id){
+        return employeeService.getEmployee(id);
+    }
+
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
@@ -40,6 +48,14 @@ public class EmployeeController {
         return new ResponseEntity<>(newEmployee, HttpStatus.NO_CONTENT);
     }
 
+    @GetMapping("employees/categories")
+    public Iterable<Employee> getEmployeesByCategory(EmployeeCategory employeecategory) {
+        return  employeeService.getEmployeesByCategory(employeecategory);
+    }
     
+    @PutMapping("employees/updatesalary")
+    public void updateSalary(long id, double salary){
+        
+    }
 
 }
